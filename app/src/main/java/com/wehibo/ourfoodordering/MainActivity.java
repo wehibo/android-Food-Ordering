2262ce7ab309a2d9bbf5089263efd40a4376ec74
wehibo <wehibkedir18@gmail.com>
2023-01-10 09:04:37 +0300

    creating an object

--- app/src/main/java/com/wehibo/ourfoodordering/MainActivity.java
+++ app/src/main/java/com/wehibo/ourfoodordering/MainActivity.java
@@ -3,12 +3,15 @@ package com.wehibo.ourfoodordering;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText name;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
