8e2ab5489e294dd4bfc5d41ec27e9d614d98a5d1
wehibo <wehibkedir18@gmail.com>
2023-01-10 14:36:06 +0300

    Update MainActivity.java

--- app/src/main/java/com/wehibo/ourfoodordering/MainActivity.java
+++ app/src/main/java/com/wehibo/ourfoodordering/MainActivity.java
@@ -1,17 +1,41 @@
 package com.wehibo.ourfoodordering;
 
+//TODO fix back button issues
+//TODO
+//TODO
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.widget.EditText;
-
 public class MainActivity extends AppCompatActivity {
-
-    EditText name;
+    
+    ListView listView;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        
+        listView = findViewById(R.id.main);
+        
+        listView.setAdapter(new com.example.myrestaurant.MenuAdapter(this, Database.categories));
+        
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                
+                Intent toFoodList = new Intent(getApplicationContext(), FoodListActivity.class);
+                toFoodList.putExtra("category", Database.categories.get(position).getCategory());
+                startActivity(toFoodList);
+            }
+        });
     }
+    
 }
